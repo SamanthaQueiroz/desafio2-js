0ea4e2e2040052e6a74c84a340c902925d3a8da1
SamanthaQueiroz <ssequeiroz@gmail.com>
2023-02-13 20:02:54 -0300

    subindo desafio 2 da coder house

---
+++ desafio2.js
@@ -0,0 +1,50 @@
+
+// TAREFA DESAFIO 2 //
+
+// PRIMEIRA PROPOSTA//
+
+/* let nomeCompleto = prompt("digite o seu nome e sobrenome")
+             
+
+ if (nomeCompleto == "Samantha Queiroz") {
+ console.log(" sim, voce é nosso cliente!");
+              
+ }else {
+ console.log("voce nao é nosso cliente")
+ } */
+
+
+ // SEGUNDA PROPOSTA //
+
+
+ /*let numberOne = 1000
+ let numberTwo = prompt ("Digite um número para comparar com 1000: ")
+ 
+ if (numberOne > numberTwo) {
+    console.log(numberOne  + " é maior que " +  numberTwo)
+ }else if (numberOne < numberTwo) {
+    console.log(numberOne  + " é menor que " +  numberTwo)
+ }else{
+    console.log("os numeros sao iguais")
+ } */
+
+ 
+// TERCEIRA PROPOSTA //
+
+
+    
+/*let idade = prompt("Digite a sua idade:")
+
+if ((idade >= 10) && (idade <= 17)) {
+    console.log ("Você só poderá entrar no  parque acompanhado de um adulto!")
+
+}
+ else if ((idade >= 18) && (idade <= 60)){
+    console.log("Você pode entrar no parque!")
+}
+else if ((idade <= 9) || (idade >= 61)){
+    console.log ("Você não poderá entrar no parque, risco de acidente.")
+} */
+
+
+// FIM DA TAREFA DESAFIO 2//
